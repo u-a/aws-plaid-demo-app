5c72888479c83c8e3b1af945ba534da51f6606de
Ushan <ushanA95@gmail.com>
2025-10-12 21:12:25 -0400

    Formatting update to the Accounts table

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -2,11 +2,24 @@ import { TableRow, TableCell } from '@aws-amplify/ui-react';
 import Currency from './Currency';
 
 export default function Account({ account }) {
+
+  const getBalanceColor = (type) => {
+    if (type === 'depository' || type === 'investment') {
+      return 'green';
+    }
+    if (type === 'loan' || type === 'credit') {
+      return 'red';
+    }
+    return 'inherit';
+  }
+
   return (
     <TableRow>
       <TableCell>{ account.name }</TableCell>
       <TableCell>
-        <Currency amount={ account.balances?.current} currency={ account.balances?.iso_currency_code }/>
+        <span style={{ color: getBalanceColor(account.type) }}>
+          <Currency amount={ account.balances?.current} currency={ account.balances?.iso_currency_code }/>
+        </span>
       </TableCell>
       <TableCell>{ account.type }</TableCell>
       <TableCell>{ account.subtype }</TableCell>
